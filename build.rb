1c3467d04e38550b405f6b77f58393c52ddd4ede
Dmitry Grigoriev <dmitry@dxfoto.ru>
2018-09-01 22:41:54 +0300

    Build script now checks dates and produces frames for each month

--- build.rb
+++ build.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'date'
 #require 'rvg/rvg'
 #include Magick
 require 'victor'
@@ -9,8 +10,8 @@ LINES    = YAML.load(File.open('lines.yaml', 'r').read)
 SECTIONS = YAML.load(File.open('sections.yaml', 'r').read)
 MARGIN = 40 # margin from outer points to image border
 
-START_DATE = '1935-01-01'
-END_DATE   = '2018-12-31'
+START_DATE = Date.new(1935, 01, 01)
+END_DATE   = Date.new(2018, 12, 31)
 
 Y_MIN = STATIONS.map{|station| station['coords']['lat']}.min.to_f.round(4) * 10000
 Y_MAX = STATIONS.map{|station| station['coords']['lat']}.max.to_f.round(4) * 10000
@@ -25,6 +26,43 @@ def geo_to_px(coords)
   return [x, y]
 end
 
+# saturating stations data with omitted values
+STATIONS.each do |station|
+  # dates of existence
+  station['dates'].each do |dt|
+    #y, m, d = dt['since'].split('-')
+    #dt['since'] = Date.new(y, m, d)
+    if dt.has_key? 'to'
+    #  y, m, d = dt['to'].split('-')
+    #  dt['to'] = Date.new(y, m, d)
+    else
+      dt['to'] = END_DATE
+    end
+  end
+  # line belongings
+  station['lines'].each do |ln|
+    if ln.has_key? 'since'
+    #  y, m, d = ln['since'].split('-')
+    #  ln['since'] = Date.new(y, m, d)
+    else
+      ln['since'] = station['dates'][0]['since']
+    end
+    if ln.has_key? 'to'
+    #  y, m, d = ln['to'].split('-')
+    #  ln['to'] = Date.new(y, m, d)
+    else
+      ln['to'] = station['dates'][-1]['to']
+    end
+  end
+end
+
+# saturating sections data with omitted values
+SECTIONS.each do |section|
+  unless section.has_key? 'to'
+    section['to'] = END_DATE
+  end
+end
+
 SECTIONS.each do |section|
   draw_coords = []
   section['coords'].split(',').each_slice(2) do |pair|
@@ -34,20 +72,35 @@ SECTIONS.each do |section|
   section['draw_coords'] = draw_coords.join(' ')
 end
 
-SECTIONS.each do |section|
-  puts section['draw_coords']
+# cycle through dates and make images
+current_date = START_DATE
+while current_date <= END_DATE do
+  puts current_date
+
+  svg = Victor::SVG.new width: WIDTH, height: HEIGHT
+
+  svg.build do
+    STATIONS.each do |s|
+      exists = false
+      s['dates'].each do |dt|
+        if dt['since'] <= current_date && dt['to'] >= current_date
+          exists = true
+        end
+      end
+      if exists
+        x, y = geo_to_px(s['coords'])
+        circle cx: x, cy: y, r: 4, fill: '#' + LINES[s['lines'][0]['name']]['color']
+      end
+    end
+    SECTIONS.each do |s|
+      if s['since'] <= current_date && s['to'] >= current_date
+        polyline points: s['draw_coords'], stroke: '#' + LINES[s['line']]['color'], fill: 'none'
+      end
+    end
+  end
+
+  svg.save current_date.strftime 'svg/metro-%Y-%m'
+  
+  current_date = current_date >> 1
 end
 
-svg = Victor::SVG.new width: WIDTH, height: HEIGHT
-
-svg.build do
-  STATIONS.each do |s|
-    x, y = geo_to_px(s['coords'])
-    circle cx: x, cy: y, r: 4, fill: '#' + LINES[s['lines'][0]['name']]['color']
-  end
-  SECTIONS.each do |s|
-    polyline points: s['draw_coords'], stroke: '#' + LINES[s['line']]['color'], fill: 'none'
-  end
-end
-
-svg.save 'metro'
